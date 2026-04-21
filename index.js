8f8c60a3338bd2349f12eb5b39677ae0176ed268
Caseywebs <kibetpalvin91@gmail.com>
2026-04-21 14:02:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ let code = require('./pair');
 
 require('events').EventEmitter.defaultMaxListeners = 1000;
 
+// Fixed: Added app.use for bodyParser before routes that need it
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/code', code);
 app.use('/pair', async (req, res, next) => {
     res.sendFile(__path + '/pair.html');
@@ -15,21 +19,33 @@ app.use('/', async (req, res, next) => {
     res.sendFile(__path + '/main.html');
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 // ✅ Changed here to bind on 0.0.0.0
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(` ██████╗ █████╗ ███████╗███████╗██╗   ██╗██████╗ ██╗  ██╗ ██████╗ ██████╗ ███████╗███████╗
-██╔════╝██╔══██╗██╔════╝██╔════╝╚██╗ ██╔╝██╔══██╗██║  ██║██╔═══██╗██╔══██╗██╔════╝██╔════╝
-██║     ███████║███████╗█████╗   ╚████╔╝ ██████╔╝███████║██║   ██║██║  ██║███████╗███████╗
-██║     ██╔══██║╚════██║██╔══╝    ╚██╔╝  ██╔══██╗██╔══██║██║   ██║██║  ██║╚════██║╚════██║
-╚██████╗██║  ██║███████║███████╗   ██║   ██████╔╝██║  ██║╚██████╔╝██████╔╝███████║███████║
- ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝   ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝╚══════╝
+    console.log(` 
+/*
 
-Don't Forget To Give Star ‼️
+ _____                                 _                 _            
+/  __ \\                               | |               | |           
+| /  \\/  __ _  ___   ___  _   _  _ __ | |__    ___    __| |  ___  ___ 
+| |     / _\` |/ __| / _ \\| | | || '__|| '_ \\  / _ \\  / _\` | / _ \\/ __|
+| \\__/\\| (_| |\\__ \\|  __/| |_| || |   | | | || (_) || (_| ||  __/\\__ \\
+ \\____/ \\__,_||___/ \\___| \\__, ||_|   |_| |_| \\___/  \\__,_| \\___||___/
+                           __/ |                                      
+                          |___/                                       
 
-made By Caseyrhodes 
+ _____              _     
+|_   _|            | |    
+  | |    ___   ___ | |__  
+  | |   / _ \\ / __|| '_ \\ 
+  | |  |  __/| (__ | | | |
+  \\_/   \\___| \\___||_| |_|
+@ Project Name : caseyrhodes tech 
+* Creator      : Caseyrhodes Tech 
+* My Git       : https//github.com/caseyweb 
+* Contact      : wa.me/254112192119
+*
+* Release Date : 18 august 2025 12.01 AM
+*/
 
 Server running on http://0.0.0.0:` + PORT);
 });
